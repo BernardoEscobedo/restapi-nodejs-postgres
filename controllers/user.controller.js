a3023ab3a7d7110d7b1e7eaf383822ec35b64e92
BernardoEscobedo <berna.lsc20@gmail.com>
2025-04-21 21:30:54 -0600

    Actualizacion

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcryptjs'
+import jwt from "jsonwebtoken"
 import {UserModel} from '../models/user.model.js'
 
 // /api/v1/users/register
@@ -6,13 +7,22 @@ const register = async(req,res) =>{
     try {
         const {username, email, password} = req.body
 
-        if(!username || !email || !password){
-            return res.status(400).json({ok: false, msg: "La solicitud requiere los siguientes datos: correo, contraseña y usuario"})
+        const missingFields = [];
+        if (!username) missingFields.push('username');
+        if (!email) missingFields.push('email');
+        if (!password) missingFields.push('password');
+
+        if (missingFields.length > 0) {
+            return res.status(400).json({
+            ok: false,
+            msg: `Faltan los siguientes campos: ${missingFields.join(', ')}`
+            });
         }
+
 
         const user = await UserModel.findOneByEmail(email)
         if(user){
-            return res.status(409).json({ok: falso, msg: "El email ya existe"})
+            return res.status(409).json({ok: false, msg: "El email ya existe"})
         }
 
         const salt = await bcrypt.genSalt(10)//se generan palabras aleatorias para la contraseña
@@ -20,13 +30,23 @@ const register = async(req,res) =>{
 
         const newUser = await UserModel.create({email, password: hashedPassword, username})
 
-        return res.status(201).json({ ok:true, msg: newUser})
+        const token = jwt.sign({
+            email: newUser.email
+        },
+            process.env.JWT_SECRET,
+            {
+                expiresIn:"1h"
+            }
+        )
+
+        return res.status(201).json({ ok:true, msg: token})
     } catch (error) {
-        console.log(error)
+        console.error(error);
+    
         return res.status(500).json({
             ok: false,
             msg: 'Error en el servidor'
-        })
+        });
     }
 }
 
