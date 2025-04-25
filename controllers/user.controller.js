d4edb6dd8ed52a102c90e1301c9a95250e21b025
BernardoEscobedo <berna.lsc20@gmail.com>
2025-04-24 20:35:40 -0600

    Actualizacion

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -30,9 +30,7 @@ const register = async(req,res) =>{
 
         const newUser = await UserModel.create({email, password: hashedPassword, username})
 
-        const token = jwt.sign({
-            email: newUser.email
-        },
+        const token = jwt.sign({email: newUser.email},
             process.env.JWT_SECRET,
             {
                 expiresIn:"1h"
@@ -53,7 +51,38 @@ const register = async(req,res) =>{
 // /api/v1/users/login
 const login = async(req,res) =>{
     try {
-        
+        const {email, password} = req.body
+
+        const missingFields = []
+        if (!email) missingFields.push('email')
+        if (!password) missingFields.push('password')
+
+        if (missingFields.length > 0) {
+            return res.status(400).json({
+            ok: false,
+            msg: `Faltan los siguientes campos: ${missingFields.join(', ')}`
+            });
+        }
+
+        const user = await UserModel.findOneByEmail(email)
+        if(!user){
+            return res.status(404).json({ok: false, msg: "Email no encontrado"})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch){
+            return res.status(401).json({ok: false, msg:"Contraseña incorrecta"})
+        }
+
+        const token = jwt.sign({email: user.email},
+            process.env.JWT_SECRET,
+            {
+                expiresIn:"1h"
+            }
+        )
+
+        return res.json({ ok:true, msg: token})
     } catch (error) {
         console.log(error)
         return res.status(500).json({
