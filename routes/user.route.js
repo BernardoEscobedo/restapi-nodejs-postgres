ebf6a58d4e3a50fdefbdab19803ee40648d40008
BernardoEscobedo <berna.lsc20@gmail.com>
2025-05-23 21:04:47 -0600

    Actualizacion

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,15 @@
 import {Router} from "express";
 import { UserController } from "../controllers/user.controller.js";
 //import { verify } from "jsonwebtoken";
-import { verifyToken } from "../middlewares/jwt.middlewares.js";
+import { verifyAdmin, verifyToken } from "../middlewares/jwt.middlewares.js";
 
 const router = Router()
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/profile', verifyToken, UserController.profile)//ruta protegida
+
+//Rutas para admin
+router.get('/', verifyToken, verifyAdmin)
 
 export default router;
